e038f3aacddcbf6eecc5492a1d09b9cd8c403807
Marcus Jessnitz <Alliance2000@iMac.local>
2017-09-10 00:38:12 -0700

    First commit

---
+++ StackSkillz/StackSkillz/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  StackSkillz
+//
+//  Created by Marcus Jessnitz on 9/10/17.
+//  Copyright © 2017 venor.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        print(text1.text!)
+        print(text2.text!)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
